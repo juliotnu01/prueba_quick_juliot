796f8d9758c9c7b721635018cefa77577bdc6d85
Juliot nuñez <nunezjuliot@gmail.com>
2021-11-23 06:33:27 -0500

    primer commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {store} from './plugins/store'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store,
+  vuetify
 }).$mount('#app')
